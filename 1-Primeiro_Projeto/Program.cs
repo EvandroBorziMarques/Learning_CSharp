189f099326e0ea368df236dbd2c1d271ba7c893e
Evandro <evandrocurriculos2017@gmail.com>
2021-03-13 23:05:16 -0300

    Start
    Create Hello World and understanding the variable int.

---
+++ 1-Primeiro_Projeto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Primeiro_Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Olá Mundo. Este é meu primeiro aplicativo!");
+
+            Console.WriteLine("A execução acabou. Tecle enter para finalizar...");
+            Console.ReadLine();
+
+        }
+    }
+}
